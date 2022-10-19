0846c66a82e32f590f09a501f9ad1146881278a2
rob <aciees95@gmail.com>
2022-10-20 01:33:40 +0900

    게시글 수정, 조회 기능 갱신

---
+++ src/test/java/com/nicearm/boardservice/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.nicearm.boardservice.springboot.domain.posts;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void board_save_load() {
+        String title = "제목";
+        String content = "본문입니다.";
+
+        postsRepository.save(Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("aciees95@gmail.com")
+                        .build());
+
+        List<Posts> postsList =  postsRepository.findAll();
+
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+}
