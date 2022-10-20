ffe82b785a85b4e923ba49e9fe7bed6a7752f681
hyundam <closer716@naver.com>
2022-10-20 17:23:07 +0900

    TimeEntity 추가 및 테스트

--- src/test/java/com/nicearm/boardservice/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/nicearm/boardservice/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,5 +40,22 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void baseTimeEntity_enroll() {
+        LocalDateTime now = LocalDateTime.of(2022, 10, 20, 17, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Posts posts = postsRepository.findAll().get(0);
+
+        System.out.println(">>>>>>>>> createDate="+posts.getCreatedDate()+
+                ", modifiedDate="+posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
--- src/test/java/com/nicearm/boardservice/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/nicearm/boardservice/springboot/web/PostsApiControllerTest.java
@@ -4,6 +4,7 @@ import com.nicearm.boardservice.springboot.domain.posts.Posts;
 import com.nicearm.boardservice.springboot.domain.posts.PostsRepository;
 import com.nicearm.boardservice.springboot.web.dto.PostsSaveRequestDto;
 
+import com.nicearm.boardservice.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -59,5 +62,59 @@ public class PostsApiControllerTest {
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void my_posts_update() throws Exception {
+        Posts posts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        Long id = posts.getId();
+        String url = "http://localhost:" + port + "/api/v1/posts/" + id;
+        String newTitle = "newTitle";
+        String newContent = "newContent";
+        restTemplate.put(url, PostsUpdateRequestDto.builder()
+                .title(newTitle)
+                .content(newContent)
+                .build());
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(newTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(newContent);
+
+    }
+    @Test
+    public void posts_update_book() throws Exception {
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        Long updateId = savedPosts.getId();
+
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        ResponseEntity<Long> responseEntity = restTemplate
+                .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+
     }
 }
