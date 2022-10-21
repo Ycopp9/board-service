5c1ac8b877645f133625c05b65eab5d9df259d6c
ycopp <aciees95@gmail.com>
2022-10-21 16:40:13 +0900

    게시글 조회 구현

--- src/main/java/com/nicearm/boardservice/springboot/service/posts/PostsService.java
+++ src/main/java/com/nicearm/boardservice/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.nicearm.boardservice.springboot.service.posts;
 
 import com.nicearm.boardservice.springboot.domain.posts.Posts;
 import com.nicearm.boardservice.springboot.domain.posts.PostsRepository;
+import com.nicearm.boardservice.springboot.web.dto.PostsListResponseDto;
 import com.nicearm.boardservice.springboot.web.dto.PostsResponseDto;
 import com.nicearm.boardservice.springboot.web.dto.PostsSaveRequestDto;
 import com.nicearm.boardservice.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,4 +38,12 @@ public class PostsService {
                         IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
 }
